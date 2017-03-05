cc84349b6e39e84792219affa8bc19bf3b506556
Jay MOULIN <jaymoulin@gmail.com>
2017-03-05 15:08:25 +0000

    Add signal handleing

--- daemon.sh
+++ daemon.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+trap 'pkill -f cups && pkill -f cloudprint && exit 0' TERM INT
 
 if [ -f /var/lib/cloudprintd/authfile.json ]
 then
     /usr/sbin/cupsd -f &
-    /usr/sbin/cloudprintd -a /var/lib/cloudprintd/authfile.json
+    /usr/sbin/cloudprintd -a /var/lib/cloudprintd/authfile.json &
+    PID=$!
+    wait $PID
 else
-    /usr/sbin/cupsd -f
+    /usr/sbin/cupsd -f &
+    PID=$!
+    wait $PID
 fi
+EXIT_STATUS=0
