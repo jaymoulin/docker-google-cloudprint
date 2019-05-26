2812fc1cfe13e9cb9e15424cd15496fbc4750912
Jay MOULIN <jaymoulin@gmail.com>
2019-05-26 19:42:56 +0200

    related #8 - redirect stderr+stdout to logs for cupsd processes

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ COPY qemu-*-static /usr/bin/
 
 FROM builder
 
-ARG VERSION=0.4.2
+ARG VERSION=0.4.4
 LABEL maintainer="Jay MOULIN <jaymoulin@gmail.com> <https://twitter.com/MoulinJay>"
 LABEL version="${VERSION}"
 EXPOSE 631
--- daemon.sh
+++ daemon.sh
@@ -3,11 +3,11 @@ trap 'pkill -f cups && pkill -f cloudprint && exit 0' TERM INT
 
 if [ -f /root/.cloudprintauth.json ]
 then
-    /usr/sbin/cupsd -f &
-    cloudprint -v > /dev/stderr
+    /usr/sbin/cupsd -f 2>&1 &
+    cloudprint -v 2>&1
     sleep 3
 else
-    /usr/sbin/cupsd -f &> /dev/stderr
+    /usr/sbin/cupsd -f 2>&1 &
     PID=$!
     wait $PID
 fi
